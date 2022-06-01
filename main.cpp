e6127d510f4740a431e4e3b3486dbf46c80129ae
cccccroge <pooopooo543@gmail.com>
2022-06-02 02:08:19 +0800

    add mac dev

--- main.cpp
+++ main.cpp
@@ -29,24 +29,24 @@ int
 main (int    argc,
       char **argv)
 {
-  // GtkApplication *app;
-  // int status;
+  GtkApplication *app;
+  int status;
 
-  // app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  // g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  // status = g_application_run (G_APPLICATION (app), argc, argv);
-  // g_object_unref (app);
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
 
-  // return status;
+  return status;
 
-  GdkDisplay * display;
-  GdkPixbuf * screenshot;
-  GdkRectangle * rectangle;
-  GListModel * monitors;
+  // GdkDisplay * display;
+  // GdkPixbuf * screenshot;
+  // GdkRectangle * rectangle;
+  // GListModel * monitors;
 
-  display = gdk_display_get_default();
-  monitors = gdk_display_get_monitors(display);
-  gdk_monitor_get_geometry(monitors[0], rectangle);
-  screenshot = gdk_pixbuf_get_from_window (root, x, y, width, height);
+  // display = gdk_display_get_default();
+  // monitors = gdk_display_get_monitors(display);
+  // gdk_monitor_get_geometry(monitors[0], rectangle);
+  // screenshot = gdk_pixbuf_get_from_window (root, x, y, width, height);
   // gdk_pixbuf_save...
 }
