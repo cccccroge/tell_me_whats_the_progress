0e9e57ab55185662e4c3399893a7f84bf6032036
cccccroge <pooopooo543@gmail.com>
2022-06-02 01:07:47 +0800

    fail to replicate deprecated api

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ activate (GtkApplication *app,
   GtkWidget *window;
   GtkWidget *button;
 
-  window = gtk_application_window_new (app);
+  window = gtk_application_window_new(app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
@@ -29,13 +29,24 @@ int
 main (int    argc,
       char **argv)
 {
-  GtkApplication *app;
-  int status;
+  // GtkApplication *app;
+  // int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+  // app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  // g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  // status = g_application_run (G_APPLICATION (app), argc, argv);
+  // g_object_unref (app);
 
-  return status;
+  // return status;
+
+  GdkDisplay * display;
+  GdkPixbuf * screenshot;
+  GdkRectangle * rectangle;
+  GListModel * monitors;
+
+  display = gdk_display_get_default();
+  monitors = gdk_display_get_monitors(display);
+  gdk_monitor_get_geometry(monitors[0], rectangle);
+  screenshot = gdk_pixbuf_get_from_window (root, x, y, width, height);
+  // gdk_pixbuf_save...
 }
